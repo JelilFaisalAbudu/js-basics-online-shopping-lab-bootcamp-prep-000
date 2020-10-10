7e7c63be2183871e1952a227662a1b18575fda7d
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-10 22:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function placeOrder(cardNumber) {
     return  "Sorry, we don't have a credit card on file for you."
   }
   else {
-    return `Your total cost is $71, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
   
   
